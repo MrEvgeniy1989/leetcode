0c37384dcfd2a2632e4a050dc1425aa5e6226e64
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-06-28 15:12:54 +0300

    40

---
+++ 40.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/explore/learn/card/fun-with-arrays/527/searching-for-items-in-an-array/3250/
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+const checkIfExist = function (arr) {
+    arr.sort((a, b) => a - b)
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] % 2 === 0) {
+            for (let j = 0; j < arr.length; j++) {
+                if (j === i) continue
+                if (arr[j] * 2 === arr[i]) return true
+            }
+        }
+    }
+    return false
+};
+
+checkIfExist([-2, 0, 10, -19, 4, 6, -8])
+checkIfExist([0, 0])
+checkIfExist([10, 2, 5, 3])
