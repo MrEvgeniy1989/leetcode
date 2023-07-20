cd15bd6306466d179ba04f129bdfce3b4fbd6898
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-07-20 17:11:58 +0300

    61

---
+++ 61.js
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/concatenation-of-array/submissions/
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const getConcatenation = function (nums) {
+    return nums.concat(nums);
+};
